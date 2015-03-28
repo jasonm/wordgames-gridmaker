d975327b54baebb5da608daebcee1ad692181c83
Jason Morrison <jmorrison@minervaproject.com>
2015-03-28 14:01:15 -0700

    search for words in grids populated randomly or by letter frequency

---
+++ index.js
@@ -0,0 +1,73 @@
+var Traverser = require("./traverser");
+var Trie = require("./trie");
+var data = require("./data.json");
+var _ = require("lodash");
+
+function lettersRandom() {
+  var letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  return letters;
+};
+
+function lettersByFrequency() {
+  var freqs = {
+    "a": 8.167, "b": 1.492, "c": 2.782, "d": 4.253, "e": 12.702, "f": 2.228,
+    "g": 2.015, "h": 6.094, "i": 6.966, "j": 0.153, "k": 0.772, "l": 4.025,
+    "m": 2.406, "n": 6.749, "o": 7.507, "p": 1.929, "q": 0.095, "r": 5.987,
+    "s": 6.327, "t": 9.056, "u": 2.758, "v": 0.978, "w": 2.360, "x": 0.150,
+    "y": 1.974, "z": 0.074,
+  }
+  var letters = _.map(freqs, function(freq, letter) {
+    return Array(1 + Math.ceil(freq * 10)).join(letter);
+  }).join('').split('');
+  return letters;
+};
+
+function gridFromLetters(letters) {
+  var grid = [];
+
+  for (var i = 0; i < 10; i++) {
+    var row = [];
+    for (var j = 0; j < 40; j++) {
+      row.push(letters[Math.floor(Math.random() * letters.length)]);
+    }
+    grid.push(row);
+  }
+  return grid;
+};
+
+var grid = gridFromLetters(lettersByFrequency());
+
+function printGrid(grid) {
+  for (var i = 0; i < grid.length; i++) {
+    console.log(grid[i].join(""));
+  }
+}
+printGrid(grid);
+
+var found = {};
+var traverser = new Traverser(5, grid, new Trie(data));
+traverser.onComplete(function() {
+  console.log(Object.keys(found).length);
+});
+traverser.search(function(word, positions) {
+  found[word] = found[word] || [];
+  found[word].push(positions);
+});
+
+// Found "word" at [ positions ]
+//
+// {
+//   "xenon": [
+//     [ [ 5, 19 ], [ 5, 18 ], [ 4, 17 ], [ 3, 18 ], [ 4, 19 ] ],
+//     [ [ 5, 19 ], [ 5, 18 ], [ 4, 19 ], [ 3, 18 ], [ 4, 17 ] ]
+//   ],
+//   "yeast": [
+//     [ [ 8, 22 ], [ 9, 23 ], [ 8, 24 ], [ 9, 25 ], [ 9, 24 ] ]
+//   ]
+// }
+
+// var keys = Object.keys(found).sort();
+// for (var i = 0; i < keys.length; i++) {
+//   console.log(keys[i]);
+//   console.log(found[keys[i]]);
+// }
